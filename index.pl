496735a4cf1c485a2fba544a653ac7620dfb7e04
Fernando Correa <fernandocorrea@gmail.com>
2012-11-13 19:25:37 -0200

    Demo

--- index.pl
+++ index.pl
@@ -1,13 +1,37 @@
 #!/usr/bin/perl 
 use Mojolicious::Lite;
+use DBI;
+use Data::Dumper;
 #use Mojolicious::Plugin::Mongodb;
+
+my $dbfile	= 	"./demo.db";
+my $db		=	DBI->connect("dbi:SQLite:dbname=$dbfile", undef, undef);
 
 get "/" => "index";
 
 post "/search" => sub {
 	my $self = shift;
-	my $search = $self->param("search") || "search";
-	$self->render_json({list => [{name => "${search}1"}, {name => "${search}2"}, {name => "${search}3"}]});
+	my $search = $self->param("search");
+	my $strquery = "select id, title from ads" . ($search ? " where title like '%$search%'" : "");
+	app->log->debug($strquery);
+	my $query = $db->prepare($strquery);
+	$query->execute;
+	my @ads;
+	my $line;
+	push(@ads, $line) while $line = $query->fetchrow_hashref; 
+	app->log->debug(Dumper(\@ads));
+	$self->render_json({list => \@ads});
+};
+
+get "/ad/:id" => sub {
+	my $self = shift;
+	my $id = $self->param("id");
+	my $strquery = "select * from ads where id = ?";
+	app->log->debug($strquery);
+	my $query = $db->prepare($strquery);
+	$query->execute($id);
+	$self->render_json($query->fetchrow_hashref);
+	$query->finish;
 };
 
 app->start;
