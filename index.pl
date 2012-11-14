86ef9a52842d4c8a75d8ab2fcac01202b8a8ea96
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-11-13 23:43:17 -0200

    add session

--- index.pl
+++ index.pl
@@ -9,7 +9,7 @@ my $db		=	DBI->connect("dbi:SQLite:dbname=$dbfile", undef, undef);
 
 get "/" => "index";
 
-post "/search" => sub {
+any "/search" => sub {
 	my $self = shift;
 	my $search = $self->param("search");
 	my $strquery = "select id, title from ads" . ($search ? " where title like '%$search%'" : "");
@@ -34,6 +34,24 @@ get "/ad/:id" => sub {
 	$query->finish;
 };
 
+put "/ad" => sub {
+	my $self = shift;
+	my $strquery = "insert into ads(title, body, price) values(?, ?, ?)";
+	app->log->debug($strquery);
+	my $query = $db->prepare($strquery);
+	$query->execute($self->param("title"), $self->param("body"), $self->param("price"));
+	$self->redirect_to("search");
+	#my $strquery = "select id, title from ads";
+	#app->log->debug($strquery);
+	#$query = $db->prepare($strquery);
+	#$query->execute;
+	#my @ads;
+	#my $line;
+	#push(@ads, $line) while $line = $query->fetchrow_hashref; 
+	#app->log->debug(Dumper(\@ads));
+	#$self->render_json({list => \@ads});
+};
+
 app->start;
 
 __DATA__
@@ -43,6 +61,7 @@ __DATA__
 	<head>
 	</head>
 	<body>
+		<a href="#" onclick='Template.renderOn("newad.jstmpl", {"title":"","body":"","price":""}, "result"); return false'>Create an Ad</a>
 		<script src="JSTemplate/Template.js"></script>
 		<form onsubmit="Template.renderOn('list.jstmpl', ['POST /search', this], 'result'); return false">
 			<input name="search">
